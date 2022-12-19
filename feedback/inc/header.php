5cd8c4a4702207a0467e18639ea51b59d905e27f
Shahroz <shahroznaveed903@gmail.com>
2022-12-19 17:18:24 +0500

    Add Database and connect it ti MyAdminSQL

--- feedback/inc/header.php
+++ feedback/inc/header.php
@@ -1,4 +1,5 @@
-!DOCTYPE html>
+<?php include 'config/database.php';?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -10,20 +11,20 @@
 <body>
   <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
     <div class="container">
-      <a class="navbar-brand" href="#">Traversy Media</a>
+      <a class="navbar-brand" href="#">FEEDBACK SYSTEM</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
           <li class="nav-item">
-            <a class="nav-link" href="/index.php">Add Feedback</a>
+            <a class="nav-link" href="index.php">Add Feedback</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/feedback.php">Past Feedback</a>
+            <a class="nav-link" href="feedback.php">Past Feedback</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/about.php">About</a>
+            <a class="nav-link" href="about.php">About</a>
           </li>
         </ul>
       </div>
